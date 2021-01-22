ee605d8356fa1456c3a2bb67164936258bd21d7f
tmtocb <kittz.esport@gmail.com>
2021-01-22 17:41:42 +0100

    add Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_232925) do
+ActiveRecord::Schema.define(version: 2021_01_22_163312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -22,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_232925) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :category do
+    name { "MyString" }
+    category_id { 1 }
+  end
+end
