b0a3b0fc2810940ddd0c5065bff72982a8ec1154
tmtocb <kittz.esport@gmail.com>
2021-01-22 01:21:17 +0100

    move styles from asset pipeline to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss") 
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
